9e897e4a3cd54c900e93d000d6ec788485767ca4
darrencruse <darren.cruse@gmail.com>
2015-12-06 18:05:34 -0600

    scripts to make it easier to catch regressions

---
+++ diffem.sh
@@ -0,0 +1,13 @@
+set -x
+diff -b macros.js macros.js.sav
+diff -b keywords-sugar.js keywords-sugar.js.sav
+diff -b examples/chatserver.js examples/chatserver.js.sav
+diff -b examples/ecstatic.js examples/ecstatic.js.sav
+diff -b examples/ifdirective.js examples/ifdirective.js.sav
+diff -b examples/nodeserver.js examples/nodeserver.js.sav
+diff -b examples/nodeserverchaining.js examples/nodeserverchaining.js.sav
+diff -b examples/sequence.js examples/sequence.js.sav
+diff -b examples/core-snippets.js examples/core-snippets.js.sav
+diff -b examples/breakout/breakout.js examples/breakout/breakout.js.sav
+diff -b examples/mandelbrot/mandelbrot.js examples/mandelbrot/mandelbrot.js.sav
+
---
+++ makem.sh
@@ -0,0 +1,14 @@
+cp macros.js macros.js.safety
+sugar macros.slisp
+cp keywords-sugar.js keywords-sugar.js.safety
+sugar keywords-sugar.sugar
+sugar examples/chatserver.slisp examples/chatserver.js
+sugar examples/ecstatic.slisp examples/ecstatic.js
+sugar examples/ifdirective.slisp examples/ifdirective.js
+sugar examples/nodeserver.slisp examples/nodeserver.js
+sugar examples/nodeserverchaining.slisp examples/nodeserverchaining.js
+sugar examples/sequence.slisp examples/sequence.js
+sugar examples/core-snippets.slisp examples/core-snippets.js
+sugar examples/breakout/breakout.slisp examples/breakout/breakout.js
+sugar examples/mandelbrot/mandelbrot.slisp examples/mandelbrot/mandelbrot.js
+
